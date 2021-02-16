ad8f72a22af071f43d973f9a75456e1caa857eb4
shinjuno <shinjuno123@naver.com>
2021-02-16 19:08:35 +0900

    model builded

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from datetime import datetime
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class User_Info(models.Model):
+    account_id = models.CharField(max_length=100,primary_key=True)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
+    major = models.CharField(max_length=30)
+    student_id = models.IntegerField()
+    user_email = models.EmailField(max_length=250)
+    college_mail = models.EmailField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    gender = models.BooleanField()
+
+def upload_profile_image(instance,filename):
+    return 'profile-image/' + str(instance.account_id) + '/' + str(datetime.today().strftime("%Y-%m-%d")) +'.jpg'
+
+class Profile(models.Model):
+    account_id = models.OneToOneField("User_Info",on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to=upload_profile_image,null=True)
+    nickname = models.CharField(max_length=15,null=True)
+    phone_number = models.CharField(max_length=15,null=True)
+    introduction = models.TextField(null=True)
+    
+
+class My_Board_list(models.Model):
+    profile_id = models.ForeignKey("Profile",on_delete=models.CASCADE)
+    board_name = models.CharField(max_length=30,null=True)
+
+
+class Bookmarked_board(models.Model):
+    profile_id = models.ForeignKey("Profile",on_delete=models.CASCADE)
+    board_name = models.CharField(max_length=30,null=True)
+
+
+class Bulletin_board(models.Model):
+    board_name = models.CharField(max_length=30)
+    profile_id = models.ForeignKey("Profile",on_delete=models.SET_NULL,null=True)
+    bulletin_board_instruction =  models.CharField(max_length=200)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=30)
+    post_contents = models.TextField(null=False)
+    bulletin_board_name = models.ForeignKey("Bulletin_board",on_delete=models.SET_NULL,null=True)
+    profile_id = models.ForeignKey("Profile",on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    post_id = models.ForeignKey("Post",on_delete=CASCADE,primary_key=True)
+    profile_id = models.ForeignKey("Profile",on_delete=models.CASCADE)
+    comment_text = models.TextField(null=False)
