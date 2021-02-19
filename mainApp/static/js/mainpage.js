99974c203a690458202ac7fab8c76053ec5ddd57
TSKwon <sange17@naver.com>
2021-02-19 18:39:48 +0900

    Add file uploader, postInformation div

--- mainApp/static/js/mainpage.js
+++ mainApp/static/js/mainpage.js
@@ -5,13 +5,25 @@ const chatClose = document.querySelector('.chatClose');
 const writePostButton = document.querySelector('#writepostBtn');
 const writePostPage = document.querySelector('.writePost');
 const postChart = document.querySelectorAll('.post');
+const postMoreView = document.querySelector('#postMoreView');
+const postInformation = document.querySelector('.postInformation');
 const profile = document.querySelector('fa-user');
+
+const arr = [
+    "",
+    "",
+    "",
+    "",
+    "",
+]
 
 // Eventlistener
 chatButton.addEventListener('click', openChat);
 chatClose.addEventListener('click', closeChat);
 writePostButton.addEventListener('click', writePost);
+postMoreView.addEventListener('click', moreviewPost);
 profile.addEventListener('click', clickProfile);
+
 
 // Function
 // 채팅방 열기
@@ -33,14 +45,29 @@ function writePost(){
         for(i=0; i<postChart.length; i++){
             postChart[i].style.display="none";
         }
-        writePostButton.value = "홈으로 이동"
-        writePostPage.style.display = "unset"
+        writePostButton.value = "홈으로 이동";
+        writePostPage.style.display = "unset";
     }else{
         for(i=0; i<postChart.length; i++){
             postChart[i].style.display="unset";
         }
-        writePostButton.value = "게시물 작성"
-        writePostPage.style.display = "none"
+        writePostButton.value = "게시물 작성";
+        writePostPage.style.display = "none";
+    }
+}
+
+// 게시물 정보 더보기
+function moreviewPost(){
+    if(postMoreView.innerHTML === "더보기"){
+        const moreview = document.createElement('p');
+        postInformation.style.height = "300px";
+        postMoreView.innerHTML = "간략히";
+        moreview.innerText = "아브라카다브라";
+        moreview.classList.add('moreview');
+        postInformation.appendChild(moreview);
+    }else{
+        postInformation.style.height = "32px";
+        postMoreView.innerHTML = "더보기";
     }
 }
 
