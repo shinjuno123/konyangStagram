1e64d26b684623a79fe4627b51880ffd39ca829f
BBIYAC <kiy7833@gmail.com>
2021-02-22 01:43:44 +0900

    Modified boardpage and Add new board button

--- mainApp/static/js/boardpage.js
+++ mainApp/static/js/boardpage.js
@@ -10,20 +10,39 @@ const QAIcon = document.getElementById('Q-icon');
 //해당 게시판으로 들어가기
 for(let postClick of postClicks){
     postClick.addEventListener('click',function(){
+        for(let postList of postLists){
+            postList.style.display = "none";
+        }
+        document.querySelector('.QAChart').style.display = "none";
         document.getElementById('logo-board-icon').style.display="flex";
+        document.querySelector('.newBoard').style.display = "none";
         document.getElementById('logo-board').innerHTML = postClick.querySelector('.postTitle').innerHTML;
         document.getElementById('posts').style.display = 'flex'; //메인 페이지처럼 해당 게시판의 게시물들 표시
         centerChart.style.display = "none";
     });
 }
 
-//문의사항 작성
-function QAFunc() {
-    centerChart.style.height = "auto";
+//문의사항 게시판
+function QAboardFunc(){
     for(let postList of postLists){
         postList.style.display = "none";
     }
-    document.querySelector('.postList').style.display = "none";
+    document.querySelector('.QAChart').style.display = "flex";
+    document.querySelector('.writePost').style.display = "none";
+    document.querySelector('.newBoard').style.display = "none";
+    document.getElementById('posts').style.display = 'none'
+    document.getElementById('logo-board-icon').style.display="flex";
+    document.getElementById('logo-board').innerHTML = document.getElementById('q2').innerHTML;
+}
+
+//문의사항 작성
+function QAFunc() {
+    for(let postList of postLists){
+        postList.style.display = "none";
+    }
+    document.querySelector('.QAChart').style.display = "none";
+    document.querySelector('.centerChart').style.display = "none";
+    document.querySelector('.newBoard').style.display = "none";
     document.querySelector('.writePost').style.display = "flex";
     document.getElementById('logo-board-icon').style.display="flex";
     document.getElementById('logo-board').innerHTML = document.getElementById('q2').innerHTML;
@@ -37,8 +56,9 @@ function boardFunc() {
 
 //Event Listener
 boardClick.addEventListener('click',boardFunc);
-QAClick.addEventListener('click',QAFunc);
-//QAIcon.addEventListener('click', QAFunc);
+//QAClick.addEventListener('click',QAFunc);
+QAClick.addEventListener('click',QAboardFunc);
+QAIcon.addEventListener('click', QAFunc);
 
 
 
@@ -76,3 +96,11 @@ for(let slider of sliders){
         slider.scrollLeft = scrollLeft - walk;
     })
 }
+
+//새 게시판 생성
+document.querySelector('.newBoard').addEventListener('click', function() {
+    document.getElementById("modal").style.display="flex";
+    document.getElementById("modal_close_btn").addEventListener('click', function() {
+        document.getElementById("modal").style.display="none";
+    })
+})
